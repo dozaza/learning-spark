3d4c239d8c450779bb7afd881f95a682862ac41a
Dozaza <baoan.feng@gmail.com>
2016-12-28 23:17:55 +0800

    add spark sql

--- build.sbt
+++ build.sbt
@@ -6,6 +6,8 @@ libraryDependencies ++= {
   Seq(
     "org.apache.spark" % "spark-core_2.10" % "2.0.2",
     "com.fasterxml.jackson.core" % "jackson-core" % "2.8.5",
-    "au.com.bytecode" % "opencsv" % "2.4"
+    "au.com.bytecode" % "opencsv" % "2.4",
+    "org.apache.hadoop" % "hadoop-common" % "2.7.3",
+    "org.apache.spark" % "spark-sql_2.10" % "2.1.0"
   )
 }
---
+++ src/main/scala/spark_sql/SparkSql.scala
@@ -0,0 +1,27 @@
+package spark_sql
+
+
+object SparkSql {
+
+  // "sql" is Hive sql, similar to native sql, ex: SELECT name, age FROM users
+  def getFromHive(sql: String): Unit = {
+    val hiveCtx = new HiveContext()
+    // rows is a RDD
+    val rows = hiveCtx.sql(sql)
+    val firstRow = rows.first
+    println(firstRow.getString(0))
+  }
+
+  // When use hive to parse json, we don't need to prepare a hive config file "hive-site.xml"
+  def getFromJson(path: String, sql: String): Unit = {
+    val hiveCtx = new HiveContext()
+    // Create a temp hive db
+    val data = hiveCtx.jsonFile(path)
+    data.registerTempTable("data")
+    val rows = hiveCtx.sql(sql)
+    val firstRow = rows.first
+    println(firstRow.getString(0))
+  }
+
+}
+
