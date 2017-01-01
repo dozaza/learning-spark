34bd58621473643cc0f25d5b14669a2828972aae
Dozaza <baoan.feng@gmail.com>
2017-01-01 23:10:32 +0800

    update accumulator

--- src/main/scala/shared_variable/Accumulator.scala
+++ src/main/scala/shared_variable/Accumulator.scala
@@ -13,7 +13,7 @@ object Accumulator {
     // So to do a count for example, we should use accumulator. See more for new api, AccumulatorV2
     val blankLines = sc.longAccumulator
 
-    rdd.flatMap { line =>
+    val values = rdd.flatMap { line =>
       if ("" == line) {
         blankLines.add(1L)
         None
@@ -27,6 +27,48 @@ object Accumulator {
       }
     }
 
+    // Here will cause calculation
+    println(blankLines.value)
+
+    values
   }
+
+  def getValidAndCount(sc: SparkContext, path: String, index: Int)(validate: String => Boolean): RDD[String] = {
+    val rdd = sc.textFile(path)
+    // Variable in each partition is standalone, so whatever changes done on a partition will never be applied on another partition.
+    // So to do a count for example, we should use accumulator. See more for new api, AccumulatorV2
+    val blankLines = sc.longAccumulator
+    val invalidNumbers = sc.longAccumulator
+
+    val values = rdd.flatMap { line =>
+      if ("" == line) {
+        blankLines.add(1L)
+        None
+      } else {
+        val values = line.split(", ")
+        if (values.size >= index) {
+          None
+        } else {
+          Some(values(index))
+        }
+      }
+    }
+
+    // Better not to use accumulator in RDD transformation, cause this accumulator may be update several times
+    // Accumulator in RDD transformation is used for debug, for example: error count.
+    val validValues = values.filter{v =>
+      val isValid = validate(v)
+      if (!isValid) {
+        invalidNumbers.add(1L)
+      }
+      isValid
+    }
+
+    println(blankLines.value)
+    println(invalidNumbers.value)
+
+    validValues
+  }
+
 
 }
